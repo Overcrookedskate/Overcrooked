407ca53cc3a02f1c645e5030045e4f6171be4abc
Luciano Naganawa <overcrookedskate@gmail.com>
2012-09-11 14:11:59 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Overcrooked::Application.config.secret_token = 'c8dd206bf1b6da08483f19788b8f13ba3183e9eec43389df3f2c25242ed634c8b86d825f4703ffbbbf7fa8dacd39ec60bfd9a0e16deaaa662aa3220b22b15b7e'
